09ea71c1bf0531f55297c54b3d53b19323f6cebb
AudiTGH <63733458+AudiTGH@users.noreply.github.com>
2020-08-29 16:17:35 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -12,7 +12,7 @@ var urls = [];
 urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
 urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
 urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
-urls.push('https://docs.google.com/spreadsheets/d/1EVR6zqtjuGV_mX0AmGxtdRdQzZ8sW4u82tR-gpTecyo/');
+urls.push('https://docs.google.com/spreadsheets/d/1EVR6zqtjuGV_mX0AmGxtdRdQzZ8sW4u82tR-gpTecyo/pub?output=csv');
 
 var number = getUrlVars()["u"];
 var googleDocURL = urls[number];
