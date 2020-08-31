4eb2b7a636231dba8346198ab5a01b065b42fe50
AudiTGH <63733458+AudiTGH@users.noreply.github.com>
2020-08-31 20:38:45 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -19,7 +19,8 @@ var number = getUrlVars()["u"];
 */
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1EVR6zqtjuGV_mX0AmGxtdRdQzZ8sW4u82tR-gpTecyo/edit#gid=0';
 //var googleApiKey = 'AIzaSyBuCCp_xVL36St8f8sn9InouC2eqMDjB8I'; // mien
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY'; // leur
+var googleApiKey = 'AIzaSyCkSgBDkgcdnoqBGPnmPqiNjhUpV_e02gI' // mien n°2
+//var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY'; // leur
 
 // ceux du site 
 //var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
