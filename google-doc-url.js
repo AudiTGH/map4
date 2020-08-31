ccec50d794b3d3bd30b991829374746e521597a9
AudiTGH <63733458+AudiTGH@users.noreply.github.com>
2020-08-31 19:03:18 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -15,6 +15,6 @@ urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_N
 urls.push('https://docs.google.com/spreadsheets/d/1EVR6zqtjuGV_mX0AmGxtdRdQzZ8sW4u82tR-gpTecyo/edit#gid=0');
 
 var number = getUrlVars()["u"];
-//var googleDocURL = urls[number]; 
-var googleDocURL ='https://docs.google.com/spreadsheets/d/1EVR6zqtjuGV_mX0AmGxtdRdQzZ8sW4u82tR-gpTecyo/edit#gid=0';
+var googleDocURL = urls[number]; 
+//var googleDocURL ='https://docs.google.com/spreadsheets/d/1EVR6zqtjuGV_mX0AmGxtdRdQzZ8sW4u82tR-gpTecyo/edit#gid=0';
 Var googleApiKey ='AIzaSyBuCCp_xVL36St8f8sn9InouC2eqMDjB8I';
