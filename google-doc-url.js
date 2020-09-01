24e7586d8ab4f0a57e932757e7a1ac99a5693e57
AudiTGH <63733458+AudiTGH@users.noreply.github.com>
2020-09-01 17:33:29 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -8,19 +8,34 @@ function getUrlVars() {
   return vars;
 }
 
-var urls = [];
+var googleApiKey = 'AIzaSyCkSgBDkgcdnoqBGPnmPqiNjhUpV_e02gI' // mien n°2
+
+//var UrlList = '1ymP0ufO4eKatKxFHC3nOmda_PZjaoqSxRdjPbBqoj7c';
+function loadUrlList() { 
+          Papa.parse('https://docs.google.com/spreadsheets/d/e/2PACX-1vRXU-Rbkwojw2hDppF_EFobsawFchtHCrPTVXdrn5k-FOI6qasCwvRppC6h-9ixt9pRuBxw4AzVJqHT/pub?gid=0&single=true&output=csv', {
+          download: true,
+          header: true,
+          complete: function(results) {
+            var urls = results.data
+            //console.log(data)
+          }
+        })
+window.addEventListener('DOMContentLoaded', loadUrlList)
+
+*/ var urls = [];
 urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
 urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
 urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
 urls.push('https://docs.google.com/spreadsheets/d/1EVR6zqtjuGV_mX0AmGxtdRdQzZ8sW4u82tR-gpTecyo/edit#gid=0');
-
+*/
+  
 var number = getUrlVars()["u"];
 var googleDocURL = urls[number]; 
 
 //var googleDocURL = 'https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0'; // ok
 //var googleDocURL = 'https://docs.google.com/spreadsheets/d/1EVR6zqtjuGV_mX0AmGxtdRdQzZ8sW4u82tR-gpTecyo/edit#gid=0'; // test
 //var googleApiKey = 'AIzaSyBuCCp_xVL36St8f8sn9InouC2eqMDjB8I'; // mien
-var googleApiKey = 'AIzaSyCkSgBDkgcdnoqBGPnmPqiNjhUpV_e02gI' // mien n°2
+
 //var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY'; // leur
 
 // ceux du site 
