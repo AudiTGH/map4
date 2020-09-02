c12502e15028bc75367da9333469fded77401135
AudiTGH <63733458+AudiTGH@users.noreply.github.com>
2020-09-02 09:50:30 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -8,14 +8,6 @@ function getUrlVars() {
   return vars;
 }
 
-
-var urls = [];
-urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
-urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
-urls.push('https://docs.google.com/spreadsheets/d/1-13FgMNZeeuIOrEVenv3qhKwEt2_NflG_7FQKGY9KoE/edit#gid=0');
-urls.push('https://docs.google.com/spreadsheets/d/1EVR6zqtjuGV_mX0AmGxtdRdQzZ8sW4u82tR-gpTecyo/edit#gid=0');
-
 var googleApiKey = 'AIzaSyCkSgBDkgcdnoqBGPnmPqiNjhUpV_e02gI' // mien n°2
 var number = getUrlVars()["u"];
-//var googleDocURL = urls[number]; 
-var googleDocURL = getUrlVars()["u"];
+var spreadsheetId  = getUrlVars()["u"];
